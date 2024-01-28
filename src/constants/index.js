54f630a3c49539f4f5f08ca486b7a8dea7d82070
TMTVato <johnlipro36@gmail.com>
2024-01-28 21:56:34 +0100

    experience component finished

--- src/constants/index.js
+++ src/constants/index.js
@@ -117,7 +117,7 @@ import {
   const experiences = [
     {
       title: "React.js Developer",
-      company_name: "Starbucks",
+      school_name: "Starbucks",
       icon: starbucks,
       iconBg: "#383E56",
       date: "March 2020 - April 2021",
@@ -130,7 +130,7 @@ import {
     },
     {
       title: "React Native Developer",
-      company_name: "Tesla",
+      school_name: "Tesla",
       icon: tesla,
       iconBg: "#E6DEDD",
       date: "Jan 2021 - Feb 2022",
@@ -143,7 +143,7 @@ import {
     },
     {
       title: "Web Developer",
-      company_name: "Shopify",
+      school_name: "Shopify",
       icon: shopify,
       iconBg: "#383E56",
       date: "Jan 2022 - Jan 2023",
@@ -156,7 +156,7 @@ import {
     },
     {
       title: "Full stack Developer",
-      company_name: "Meta",
+      school_name: "Meta",
       icon: meta,
       iconBg: "#E6DEDD",
       date: "Jan 2023 - Present",
