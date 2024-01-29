7fde9b5275d7174dbdfd0cd50d840e689efd91f9
TMTVato <johnlipro36@gmail.com>
2024-01-29 22:36:39 +0100

    emailjs + correction flex contact + navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,36 @@ import { logo, menu, close } from "../assets";
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
     <nav
-    className={`${ styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 `}>
+    className={`${ styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${
+      scrolled ? "bg-primary" : "bg-transparent"
+    }`}>
       <div className="w-full flex items-center justify-between max-w-7xl mx-auto">  
         <Link to="/" className="flex items-center gap-2" 
         onClick={()=>{setActive("");
         window.scrollTo(0,0);}}
         >
            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+          <p className='text-[#915EFF] text-[20px] font-bold cursor-pointer flex '>
             John &nbsp;
             <span className='sm:block hidden'>| CS student</span>
           </p>
@@ -26,7 +46,7 @@ const Navbar = () => {
           {navLinks.map((nav)=>(<li
           key={nav.id} className={`${
             active === nav.title ? "text-white" : "text-secondary"
-          } hover:text-white text-[18px] font-medium cursor-pointer`}
+          } hover:text-[#915EFF] text-[20px] font-medium cursor-pointer`}
           onClick={() => setActive(nav.title)}
 
         ><a href={`#${nav.id}`}>{nav.title}</a></li>))}
